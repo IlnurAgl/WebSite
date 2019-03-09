5efb7ff70e22541a73db5f9ac7b955305f973a17
Ilnur <ilnur5247@gmail.com>
2019-03-10 01:43:04 +0300

    second

--- main/main/routes.py
+++ main/main/routes.py
@@ -12,7 +12,14 @@ main = Blueprint('main', __name__)
 def home():
     page = request.args.get('page', 1, type=int)
     posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
-    return render_template('home.html', posts=posts)
+    return render_template('home.html', posts=posts, reverse=False)
+
+
+@main.route('/home_reverse')
+def home_reverse():
+	page = request.args.get('page', 1, type=int)
+	posts = Post.query.paginate(page=page, per_page=5)
+	return render_template('home.html', posts=posts, reverse=True)
 
 
 # Страница о сайте
--- main/users/routes.py
+++ main/users/routes.py
@@ -97,6 +97,16 @@ def user_posts(username):
     return render_template('user_posts.html', posts=posts, user=user)
 
 
+@users.route("/user_reverse/<string:username>")
+def user_reverse_posts(username):
+    page = request.args.get('page', 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = Post.query.filter_by(author=user)\
+        .order_by(Post.date_posted.desc())\
+        .paginate(page=page, per_page=5)
+    return render_template('user_posts_reverse.html', posts=posts, user=user)
+
+
 # Страница для изменения пароля
 @users.route("/reset_password", methods=['GET', 'POST'])
 def reset_request():
